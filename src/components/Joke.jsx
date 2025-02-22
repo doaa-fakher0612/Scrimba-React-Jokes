6391581ae8359ba00c35ea686772182245448139
doaa <doaafakher0612@gmail.com>
2025-02-22 17:18:43 +0200

    ternary operator vs conditional rendering

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 export default function Joke(props){
+
+   
     // console.log(props)
     const [isShown, setIsShown] = useState(false)
-    console.log(isShown)
+    // console.log(isShown)
     function showPunchline(){
         setIsShown (prevShown => !prevShown)
      
@@ -16,9 +18,16 @@ export default function Joke(props){
         /* conditional render  if (props.setup) is truthy ? (<h3>Setup: {props.setup}</h3>)will ber rendered
         if falsy => nothing will be rendered*/}
          {props.setup && <h3>Setup: {props.setup}</h3>}
-         {isShown && <p>punchline: {props.punchline}</p>}
+
+         {/* {isShown && <p>punchline: {props.punchline}</p>} */}
+         {/* ternary operator */}
+         {isShown ? <p>punchline: {props.punchline}</p> : null}
          <button onClick={showPunchline}>{!isShown ? "Show Punchline" : "Hide Punchline"}</button>
          <hr />
         </>
     )
+}
+
+if (true && console.log("This code is running")) {
+    console.log("Everything was true!")
 }
