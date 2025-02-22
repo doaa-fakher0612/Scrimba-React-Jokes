373fb8198e72bbb1916ac4c927d5807ee4a055b6
doaa <doaafakher0612@gmail.com>
2025-02-22 16:35:45 +0200

    conditional rendering in button value , and show punchline >>2

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Joke(props){
-    console.log(props)
+    // console.log(props)
+    const [isShown, setIsShown] = useState(false)
+    console.log(isShown)
+    function showPunchline(){
+        setIsShown (prevShown => !prevShown)
+     
+    }
     return (
         <>
         {
-        /* conditional render  if (props.setup) is truthy ? (<h3>Setup: {props.setup}</h3>)will ber rendered
-        if falsy => nothing woll be rendered*/}
+          
 
+        /* conditional render  if (props.setup) is truthy ? (<h3>Setup: {props.setup}</h3>)will ber rendered
+        if falsy => nothing will be rendered*/}
          {props.setup && <h3>Setup: {props.setup}</h3>}
-         <p>punchline: {props.punchline}</p>
+         {isShown && <p>punchline: {props.punchline}</p>}
+         <button onClick={showPunchline}>{!isShown ? "Show Punchline" : "Hide Punchline"}</button>
          <hr />
         </>
     )
